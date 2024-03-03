a7325dbc5b0d47b1265bd1a87db18ded9a76299a
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-03-03 23:23:39 +0300

    add generate pdf and upd workflow

--- generate-pdf.py
+++ generate-pdf.py
@@ -1,7 +1,7 @@
 import requests
 import os
 
-key = os.environ['ILOVEPDF_API_TOKEN']
+key = os.environ['ILOVEPDF_API_KEY']
 
 start = requests.get(' https://api.ilovepdf.com/v1/start/htmlpdf', headers={'Authorization': 'Bearer {key}'}).json()
 
