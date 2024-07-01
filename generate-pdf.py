9956a71b90861ddc5904a2fa3981e7c4455895ec
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-07-01 12:48:51 +0300

    upd resume

--- generate-pdf.py
+++ generate-pdf.py
@@ -3,6 +3,6 @@ import os
 
 key = os.environ['ILOVEPDF_API_KEY']
 
-start = requests.get(' https://api.ilovepdf.com/v1/start/htmlpdf', headers={'Authorization': 'Bearer {key}'}).json()
+start = requests.get('https://api.ilovepdf.com/v1/start/htmlpdf', headers={'Authorization': f'Bearer {key}'}).json()
 
 print(start)
