eb31be7a3d0e83511d22e10f4d6a1905de730f35
Vyacheslav Zalygin <vya.zalygin@gmail.com>
2024-09-13 11:21:05 +0300

    upload updated resume

--- generate-pdf.py
+++
@@ -1,8 +0,0 @@
-import requests
-import os
-
-key = os.environ['ILOVEPDF_API_KEY']
-
-start = requests.get('https://api.ilovepdf.com/v1/start/htmlpdf', headers={'Authorization': f'Bearer {key}'}).json()
-
-print(start)
